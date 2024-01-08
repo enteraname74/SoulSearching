2ffd9eebd49db48d463b0bf9fd4b429f75905419
enteraname74 <noahpenin@gmail.com>
2024-01-08 02:35:11 +0100

    Start working on multiplatform strings

--- shared-ui/src/androidMain/kotlin/com/github/soulsearching/composables/NoElementView.kt
+++ shared-ui/src/androidMain/kotlin/com/github/soulsearching/composables/NoElementView.kt
@@ -8,12 +8,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.github.soulsearching.R
 import com.github.soulsearching.ui.theme.DynamicColor
+import com.github.soulsearching.utils.strings
 
 @Composable
 fun NoElementView(
@@ -27,7 +26,7 @@ fun NoElementView(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = stringResource(id = R.string.no_elements),
+            text = strings.noElements,
             fontSize = 15.sp,
             color = DynamicColor.subText
         )
---
+++ shared-ui/src/commonMain/kotlin/com/github/soulsearching/utils/strings.kt
@@ -0,0 +1,21 @@
+package com.github.soulsearching.utils
+
+import androidx.compose.ui.text.intl.Locale
+
+val strings = when(Locale.current.language) {
+    "fr" -> FrStrings
+    else -> EnStrings
+}
+
+interface Strings {
+    val appName: String get() = "Soul Searching"
+    val noElements: String
+}
+
+object FrStrings : Strings {
+    override val noElements = "Aucun élément"
+}
+
+object EnStrings : Strings {
+    override val noElements = "No elements"
+}
