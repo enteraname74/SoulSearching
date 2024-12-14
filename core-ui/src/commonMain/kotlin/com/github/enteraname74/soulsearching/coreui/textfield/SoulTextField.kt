265f2ba2ee6e9ecda467d516e37b2a49922932f0
enteraname74 <noahpenin@gmail.com>
2024-12-14 19:49:03 +0100

    Continue working on new textfield design

--- core-ui/src/commonMain/kotlin/com/github/enteraname74/soulsearching/coreui/textfield/SoulTextField.kt
+++ core-ui/src/commonMain/kotlin/com/github/enteraname74/soulsearching/coreui/textfield/SoulTextField.kt
@@ -11,11 +11,14 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -48,78 +51,110 @@ fun SoulTextField(
     val interactionSource = remember { MutableInteractionSource() }
     val isFocused by interactionSource.collectIsFocusedAsState()
 
-    SoulTextFieldViewHolder(
-        colors = colors,
-        style = style,
-        isFocused = isFocused,
+    val textSelectionColors = TextSelectionColors(
+        handleColor = colors.selectionContentColor,
+        backgroundColor = colors.selectionContainerColor,
+    )
+
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides textSelectionColors
     ) {
-        BasicTextField(
-            modifier = modifier,
-            value = value,
-            onValueChange = onValueChange,
-            interactionSource = interactionSource,
-            maxLines = 1,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = keyboardType,
-            ),
-            textStyle = UiConstants.Typography.bodyLarge.copy(color = colors.contentColor),
-            keyboardActions = keyboardActions,
-            cursorBrush = SolidColor(colors.contentColor),
-            decorationBox = { innerTextField ->
-                TextFieldDefaults.DecorationBox(
-                    isError = isInError,
-                    enabled = true,
-                    value = value,
-                    singleLine = true,
-                    interactionSource = interactionSource,
-                    visualTransformation = VisualTransformation.None,
-                    contentPadding = PaddingValues(
-                        vertical = UiConstants.Spacing.mediumPlus,
-                        horizontal = UiConstants.Spacing.mediumPlus,
-                    ),
-                    trailingIcon = trailingIcon,
-                    innerTextField = {
-                        Column(
-                            verticalArrangement = Arrangement.spacedBy(UiConstants.Spacing.small)
-                        ) {
-                            labelName?.let {
-                                Text(
-                                    text = it,
-                                    style = UiConstants.Typography.bodyVerySmall.copy(color = colors.labelColor),
-                                )
-                            }
-                            innerTextField()
-                            AnimatedVisibility(
-                                enter = slideInVertically(),
-                                exit = slideOutVertically(),
-                                visible = isInError,
-                            ) {
-                                Text(
-                                    text = error.orEmpty(),
-                                    style = UiConstants.Typography.bodyVerySmall.copy(color = colors.labelColor),
-                                )
-                            }
-                        }
-                    },
-                    colors = TextFieldDefaults.colors(
-                        focusedContainerColor = Color.Transparent,
-                        unfocusedContainerColor = Color.Transparent,
-                        errorContainerColor = Color.Transparent,
-                        disabledContainerColor = Color.Transparent,
-                        focusedIndicatorColor = Color.Transparent,
-                        disabledIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                        errorIndicatorColor = Color.Transparent,
-                        focusedTextColor = colors.contentColor,
-                        unfocusedTextColor = colors.contentColor,
-                        selectionColors = TextSelectionColors(
-                            handleColor = colors.selectionContentColor,
-                            backgroundColor = colors.selectionContainerColor,
+        SoulTextFieldViewHolder(
+            colors = colors,
+            style = style,
+            isFocused = isFocused,
+        ) {
+//        Column {
+//            labelName?.let {
+//                Text(
+//                    text = it,
+//                    style = UiConstants.Typography.bodyVerySmall.copy(color = colors.labelColor),
+//                )
+//            }
+//            TextField(
+//                modifier = modifier,
+//                value = value,
+//                onValueChange = onValueChange,
+//            )
+//            AnimatedVisibility(
+//                enter = slideInVertically(),
+//                exit = slideOutVertically(),
+//                visible = isInError,
+//            ) {
+//                Text(
+//                    text = error.orEmpty(),
+//                    style = UiConstants.Typography.bodyVerySmall.copy(color = colors.labelColor),
+//                )
+//            }
+//        }
+            BasicTextField(
+                modifier = modifier,
+                value = value,
+                onValueChange = onValueChange,
+                interactionSource = interactionSource,
+                maxLines = 1,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = keyboardType,
+                ),
+                textStyle = UiConstants.Typography.bodyLarge.copy(color = colors.contentColor),
+                keyboardActions = keyboardActions,
+                cursorBrush = SolidColor(colors.contentColor),
+                decorationBox = { innerTextField ->
+                    TextFieldDefaults.DecorationBox(
+                        isError = isInError,
+                        enabled = true,
+                        value = value,
+                        singleLine = true,
+                        interactionSource = interactionSource,
+                        visualTransformation = VisualTransformation.None,
+                        contentPadding = PaddingValues(
+                            vertical = UiConstants.Spacing.mediumPlus,
+                            horizontal = UiConstants.Spacing.mediumPlus,
                         ),
+                        trailingIcon = trailingIcon,
+                        innerTextField = {
+                            Column(
+                                verticalArrangement = Arrangement.spacedBy(UiConstants.Spacing.small)
+                            ) {
+                                labelName?.let {
+                                    Text(
+                                        text = it,
+                                        style = UiConstants.Typography.bodyVerySmall.copy(color = colors.labelColor),
+                                    )
+                                }
+                                innerTextField()
+                                AnimatedVisibility(
+                                    enter = slideInVertically(),
+                                    exit = slideOutVertically(),
+                                    visible = isInError,
+                                ) {
+                                    Text(
+                                        text = error.orEmpty(),
+                                        style = UiConstants.Typography.bodyVerySmall.copy(color = colors.labelColor),
+                                    )
+                                }
+                            }
+                        },
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent,
+                            errorContainerColor = Color.Transparent,
+                            disabledContainerColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            errorIndicatorColor = Color.Transparent,
+                            focusedTextColor = colors.contentColor,
+                            unfocusedTextColor = colors.contentColor,
+                            selectionColors = TextSelectionColors(
+                                handleColor = colors.selectionContentColor,
+                                backgroundColor = colors.selectionContainerColor,
+                            ),
+                        )
                     )
-                )
-            },
-        )
+                },
+            )
+        }
     }
 }
 
