b66e4ed1cdb234e393f8e15d1c34609b973d99e0
enteraname74 <noahpenin@gmail.com>
2024-12-14 20:44:56 +0100

    Fix multiline textfield bug

--- core-ui/src/commonMain/kotlin/com/github/enteraname74/soulsearching/coreui/textfield/SoulTextField.kt
+++ core-ui/src/commonMain/kotlin/com/github/enteraname74/soulsearching/coreui/textfield/SoulTextField.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -93,6 +92,7 @@ fun SoulTextField(
                 onValueChange = onValueChange,
                 interactionSource = interactionSource,
                 maxLines = 1,
+                singleLine = true,
                 keyboardOptions = KeyboardOptions(
                     keyboardType = keyboardType,
                 ),
