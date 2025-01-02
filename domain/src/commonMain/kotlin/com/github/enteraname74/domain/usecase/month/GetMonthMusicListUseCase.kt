78029e472db739e8a4cf8f0e8fa305aa0303e2f0
enteraname74 <noahpenin@gmail.com>
2025-01-02 14:02:10 +0100

    Fix music by month cover not being the same between preview and detail

--- domain/src/commonMain/kotlin/com/github/enteraname74/domain/usecase/month/GetMonthMusicListUseCase.kt
+++ domain/src/commonMain/kotlin/com/github/enteraname74/domain/usecase/month/GetMonthMusicListUseCase.kt
@@ -1,18 +1,24 @@
 package com.github.enteraname74.domain.usecase.month
 
 import com.github.enteraname74.domain.model.MonthMusicList
+import com.github.enteraname74.domain.model.SortDirection
+import com.github.enteraname74.domain.model.SortType
 import com.github.enteraname74.domain.usecase.music.GetAllMusicUseCase
+import com.github.enteraname74.domain.usecase.music.GetAllMusicsSortedUseCase
 import com.github.enteraname74.domain.util.DateUtils
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.mapLatest
 
 class GetMonthMusicListUseCase(
-    private val getAllMusicsUseCase: GetAllMusicUseCase,
+    private val getAllMusicsSortedUseCase: GetAllMusicsSortedUseCase,
 ) {
     @OptIn(ExperimentalCoroutinesApi::class)
     operator fun invoke(month: String): Flow<MonthMusicList?> =
-        getAllMusicsUseCase().mapLatest { allMusics ->
+        getAllMusicsSortedUseCase(
+            sortDirection = SortDirection.DESC,
+            sortType = SortType.ADDED_DATE,
+        ).mapLatest { allMusics ->
             val musics = allMusics.filter { DateUtils.getMonthAndYearOfDate(date = it.addedDate) == month  }
             MonthMusicList(
                 month = month,
