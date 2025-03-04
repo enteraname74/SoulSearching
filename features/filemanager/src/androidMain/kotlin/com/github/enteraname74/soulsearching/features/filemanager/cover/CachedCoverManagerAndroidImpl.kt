689de5ae4d9e972f68e5e3736215d6d77418bbc4
enteraname74 <noahpenin@gmail.com>
2025-03-04 13:35:05 +0100

    Add possibility to retrieve music file cover as bytearray

--- features/filemanager/src/androidMain/kotlin/com/github/enteraname74/soulsearching/features/filemanager/cover/CachedCoverManagerAndroidImpl.kt
+++ features/filemanager/src/androidMain/kotlin/com/github/enteraname74/soulsearching/features/filemanager/cover/CachedCoverManagerAndroidImpl.kt
@@ -7,12 +7,24 @@ import org.jetbrains.compose.resources.decodeToImageBitmap
 
 class CachedCoverManagerAndroidImpl : CachedCoverManager() {
     @OptIn(ExperimentalResourceApi::class)
-    override fun fetchCoverOfMusicFile(musicPath: String): ImageBitmap? =
+    override suspend fun fetchCoverOfMusicFile(musicPath: String): ImageBitmap? =
         try {
             val mediaMetadataRetriever = MediaMetadataRetriever()
             mediaMetadataRetriever.setDataSource(musicPath)
             val data: ImageBitmap? = mediaMetadataRetriever.embeddedPicture?.decodeToImageBitmap()
             data?.let { cachedImages[musicPath] = it }
+            data
+        } catch (e: Exception) {
+            println("CoverUtilsImpl -- Error while fetching song's cover: ${e.message}")
+            null
+        }
+
+    override suspend fun fetchCoverOfMusicFileAsByteArray(musicPath: String): ByteArray? =
+        try {
+            val mediaMetadataRetriever = MediaMetadataRetriever()
+            mediaMetadataRetriever.setDataSource(musicPath)
+            val data: ByteArray? = mediaMetadataRetriever.embeddedPicture
+            data?.let { cachedImagesByteArray[musicPath] = it }
             data
         } catch (e: Exception) {
             println("CoverUtilsImpl -- Error while fetching song's cover: ${e.message}")
