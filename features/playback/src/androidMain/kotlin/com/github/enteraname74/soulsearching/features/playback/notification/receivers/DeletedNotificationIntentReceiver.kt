745f7af06a189eeacbc90041250d8afe8d76ebd6
enteraname74 <noahpenin@gmail.com>
2025-12-06 15:43:12 +0100

    Add favorite toggle in notification

--- features/playback/src/androidMain/kotlin/com/github/enteraname74/soulsearching/features/playback/notification/receivers/DeletedNotificationIntentReceiver.kt
+++ features/playback/src/androidMain/kotlin/com/github/enteraname74/soulsearching/features/playback/notification/receivers/DeletedNotificationIntentReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.github.enteraname74.soulsearching.features.playback.notification.impl.SoulSearchingAndroidNotification
+import com.github.enteraname74.soulsearching.features.playback.notification.impl.SoulSearchingNotificationBelowAndroid13
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,7 +16,7 @@ class DeletedNotificationIntentReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         CoroutineScope(Dispatchers.IO).launch {
             val intentForNotification = Intent(SoulSearchingAndroidNotification.BROADCAST_NOTIFICATION)
-            intentForNotification.putExtra(SoulSearchingAndroidNotification.STOP_RECEIVE, true)
+            intentForNotification.putExtra(SoulSearchingNotificationBelowAndroid13.STOP_RECEIVE, true)
             context.sendBroadcast(intentForNotification)
         }
     }
--- features/playback/src/androidMain/kotlin/com/github/enteraname74/soulsearching/features/playback/notification/receivers/NextMusicNotificationReceiver.kt
+++ features/playback/src/androidMain/kotlin/com/github/enteraname74/soulsearching/features/playback/notification/receivers/NextMusicNotificationReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.github.enteraname74.soulsearching.features.playback.notification.impl.SoulSearchingAndroidNotification
+import com.github.enteraname74.soulsearching.features.playback.notification.impl.SoulSearchingNotificationBelowAndroid13
 
 /**
  * Receiver for playing the next song in the queue.
@@ -11,7 +12,7 @@ import com.github.enteraname74.soulsearching.features.playback.notification.impl
 class NextMusicNotificationReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         val intentForNotification = Intent(SoulSearchingAndroidNotification.BROADCAST_NOTIFICATION)
-        intentForNotification.putExtra(SoulSearchingAndroidNotification.NEXT, true)
+        intentForNotification.putExtra(SoulSearchingNotificationBelowAndroid13.NEXT, true)
         context.sendBroadcast(intentForNotification)
     }
 }
