e06a3c3d4dc3c39ae0ce01915a5c15e9259e0338
enteraname74 <noahpenin@gmail.com>
2026-03-19 20:48:20 +0100

    Made album position the default option

--- domain/src/commonMain/kotlin/com/github/enteraname74/domain/model/settings/SoulSearchingSettings.kt
+++ domain/src/commonMain/kotlin/com/github/enteraname74/domain/model/settings/SoulSearchingSettings.kt
@@ -231,7 +231,7 @@ object SoulSearchingSettingsKeys {
     object Album {
         val SHOULD_SHOW_TRACK_POSITION_IN_ALBUM_VIEW = settingElementOf(
             key = "SHOULD_SHOW_TRACK_POSITION_IN_ALBUM_VIEW",
-            defaultValue = false,
+            defaultValue = true,
         )
     }
 }
