8247fabc2d55d5a4135b1a423d8e9859dd25e48a
enteraname74 <noahpenin@gmail.com>
2026-07-20 19:40:42 +0200

    Small UI adjustements

--- app/src/commonMain/kotlin/com/github/enteraname74/soulsearching/feature/player/presentation/composable/PlayerMinimisedMainInfo.kt
+++ app/src/commonMain/kotlin/com/github/enteraname74/soulsearching/feature/player/presentation/composable/PlayerMinimisedMainInfo.kt
@@ -88,53 +88,12 @@ fun PlayerMinimisedMainInfo(
         if (PlayerUiUtils.canShowSidePanel()) {
             PlayerVolume(
                 modifier = Modifier
-                    .weight(1f, fill = true),
+                    .weight(1f, fill = true)
+                    .padding(
+                        end = UiConstants.Spacing.medium,
+                    ),
                 playbackCommandsState = playbackCommandsState,
             )
         }
-    }
-}
-
-@Composable
-private fun PlayerVolume(
-    playbackCommandsState: PlaybackCommandsState,
-    modifier: Modifier = Modifier,
-) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.End,
-    ) {
-        SoulIcon(
-            color = SoulSearchingColorTheme.colorScheme.onSecondary,
-            icon = when {
-                playbackCommandsState.playerVolume == 0f -> CoreRes.drawable.ic_volume_mute
-                playbackCommandsState.playerVolume <= 0.5f -> CoreRes.drawable.ic_volume_down
-                else -> CoreRes.drawable.ic_volume_up
-            }
-        )
-        SoulSlider(
-            modifier = Modifier
-                .width(150.dp),
-            minValue = 0f,
-            maxValue = 10f,
-            value = playbackCommandsState.playerVolume * 10,
-            sliderColors = SliderDefaults.colors(
-                thumbColor = SoulSearchingColorTheme.colorScheme.onSecondary,
-                activeTrackColor = SoulSearchingColorTheme.colorScheme.onSecondary,
-                inactiveTrackColor = SoulSearchingColorTheme.colorScheme.primary,
-                activeTickColor = SoulSearchingColorTheme.colorScheme.onSecondary,
-                inactiveTickColor = SoulSearchingColorTheme.colorScheme.primary,
-            ),
-            onThumbDragged = { playerVolume ->
-                playerVolume?.let {
-                    val fixedVolume = (it / 10).coerceIn(0f, 1f)
-                    playbackCommandsState.setPlayerVolume(fixedVolume)
-                }
-            },
-            onValueChanged = {
-                // update done on drag
-            }
-        )
     }
 }
